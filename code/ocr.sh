7c6021fb108e4fe7129ccb3b73cc5c585d97181d
Saber Sheybani <sheybani.saber@gmail.com>
2017-04-26 22:03:53 -0400

    Corrected a file

--- code/ocr.sh
+++ code/ocr.sh
@@ -5,11 +5,11 @@ echo "[defaults]
 host_key_checking = False" >> ~/ocr/code/ansiblescript/ansible.cfg
 mkdir ~/ocr/ocr_results
 read -p "Enter the image URL: " url1
-url_str = "image_url: $url1"
-echo url_str >> ~/ocr/code/ansiblescript/roles/run_ocr/vars/main.yml
+url_str="image_url: $url1"
+echo $url_str >> ~/ocr/code/ansiblescript/roles/run_ocr/vars/main.yml
 ansible-playbook -i ~/ocr/code/ansiblescript/inventory.txt ~/ocr/code/ansiblescript/ocr.yml
 echo "Benchmarking finished"
-echo "Accuracy in % = "
+echo "Characters are: "
 for file in ~/ocr/ocr_results/*
 do
   cat "$file"
